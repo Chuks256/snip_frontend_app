19f97f47b4b088200e4b8325aa2ed7ccc1480c51
chuks <alexanderchuks310@gmail.com>
2024-05-21 05:22:06 +0100

    worked on responsiveness

--- src/components/Errorcomponent.jsx
+++ src/components/Errorcomponent.jsx
@@ -4,9 +4,11 @@ import "../ErrorStyle.css"
 function Errorcomponent(props){
     return(
         <>
+        <div className="error_parent_container">
         <div style={{"transform":`translateY(${props.yAxis})`,"transition":"linear,500ms"}} className="errorContainer">
         <div className="errorBox">
             <div className="errorDetails">Failed to Connect</div>
+        </div>
         </div>
         </div>
         </>
